dc930506e7a3737b37e3dbc83ea52a425ad2a2d9
ymolchanov <0x4d59@gmail.com>
2017-05-25 12:29:15 -0400

    12

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const nodemailer = require('nodemailer');
 const config = require('./config/config.js')
+const http = require('http');
+const PORT = 3000;
+const server = http.createServer();
 
 let transporter = nodemailer.createTransport(
     {
@@ -11,16 +14,39 @@ let transporter = nodemailer.createTransport(
     }
 );
 
-let mailOptions = {
-    from: config.sender,
-    to: config.receiver,
-    subject: 'Test',
-    text: 'Supertest'
-};
+function sendMail(title, body) {
 
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
+    let mailOptions = {
+        from: config.sender,
+        to: config.receiver,
+        subject: title,
+        text: body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message is sent');
+    });
+
+}
+
+server.on('request', (req) => {
+    console.log('requested');
+    let body = [];
+    if (req.method === 'POST') {
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            body = JSON.parse(Buffer.concat(body).toString());
+            sendMail(body.title, body.mailBody);
+        });
     }
-    console.log('Message is sent');
+
+});
+
+server.listen(PORT, () => {
+    console.log('server started on {$PORT} port');
 });
