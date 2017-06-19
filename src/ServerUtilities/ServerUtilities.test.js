246875793d806545991f0f9eb172bdd4a942a413
ymolchanov <0x4d59@gmail.com>
2017-06-19 11:13:16 -0400

    #28 add tests for ServerUtilities#getQueryParams

--- src/ServerUtilities/ServerUtilities.test.js
+++ src/ServerUtilities/ServerUtilities.test.js
@@ -1,6 +1,7 @@
 const serverUtilities = require('./ServerUtilities.js');
 const assert = require('assert');
 const MockReq = require('mock-req');
+const http = require('http');
 
 describe('ServerUtilities', function() {
     describe('#getPostRequestBody', function() {
@@ -35,5 +36,40 @@ describe('ServerUtilities', function() {
                     }
                 );
         });
+    });
+
+    describe('#getQueryParams', function() {
+        it('parse query params and return object', function() {
+
+            let request = new http.IncomingMessage;
+            request.method = 'GET';
+            request.url = '/test?text=testing';
+
+            return serverUtilities.getQueryParams(request)
+                .then(
+                    (body) => {
+                        assert.deepEqual(body, {text: 'testing'});
+                    },
+                    (error) => {
+                        assert.ifError(error);
+                    }
+                );
+        });
+        it('must throw Error if request isn\'t instance of http.IncomingMessage', function() {
+            let request = new MockReq({
+                method: 'GET',
+                url: '/test?text=testing'
+            });
+
+            return serverUtilities.getQueryParams(request)
+                .then(
+                    (body) => {
+                        assert.fail('request parsed', 'must throw error instead', undefined, '>');
+                    },
+                    (error) => {
+                        assert.ok(error);
+                    }
+                );
+        })
     })
 });
